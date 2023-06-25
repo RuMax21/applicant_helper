a8e8c7529b985ab9fd06b1e0a11992b6f4f82e95
Max <108620734+RuMax21@users.noreply.github.com>
2023-06-25 20:52:19 +0300

    fix of working db & settings of analytics

--- analytics_bot_main.py
+++ analytics_bot_main.py
@@ -9,10 +9,14 @@ bot = telebot.TeleBot(config.TOKEN_ANALYTICS)
 
 @bot.message_handler(commands=['start'])
 def start(message):
-	if (db_management.is_user_checking(db_management.hashing_of_name(str(message.chat.id)))) & db_management.user_is_admin(db_management.hashing_of_name(str(message.chat.id))):
-		markup = main_menu()
-		bot.send_message(message.chat.id, 'Какая информация Вас интересует?', reply_markup=markup)
-	else:
+	try:
+		print(db_management.user_is_admin(db_management.hashing_of_name(str(message.chat.id))))
+		if (db_management.is_user_checking(db_management.hashing_of_name(str(message.chat.id)))) & db_management.user_is_admin(db_management.hashing_of_name(str(message.chat.id))):
+			markup = main_menu()
+			bot.send_message(message.chat.id, 'Какая информация Вас интересует?', reply_markup=markup)
+		else:
+			bot.send_message(message.chat.id, 'Недостаточно прав!')
+	except IndexError:
 		bot.send_message(message.chat.id, 'Недостаточно прав!')
 
 def main_menu():
@@ -20,13 +24,13 @@ def main_menu():
 	markup.add(
 		telebot.types.InlineKeyboardButton('Общие количество запросов', callback_data=messages.CALLBACK_BUTTON_TOTAL_REQUESTS),
 		telebot.types.InlineKeyboardButton('Количество пользователей', callback_data=messages.CALLBACK_BUTTON_NUMBER_OF_USERS),
-		telebot.types.InlineKeyboardButton('Выдать права на просмотр', callback_data='number_of_users')
-
+		telebot.types.InlineKeyboardButton('Выдать права на просмотр', callback_data='number_of_users'),
 		)
 	return markup
 
 @bot.callback_query_handler(func=lambda call: call.data.startswith(messages.CALLBACK_BUTTON_TOTAL_REQUESTS))
 def total_requests_callback(call):
+	markup = create_button_back()
 	list_of_statistics = db_management.calculate_total_requests()
 	bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id,
 		text=
@@ -37,12 +41,29 @@ def total_requests_callback(call):
 		f"Расписание: '{list_of_statistics[0][4]}' \n"
 		f"Важные даты: '{list_of_statistics[0][5]}' \n"
 		f"Стоимость обучения: '{list_of_statistics[0][5]}' \n"
-	)
+	, reply_markup = markup)
 
 @bot.callback_query_handler(func=lambda call: call.data.startswith(messages.CALLBACK_BUTTON_NUMBER_OF_USERS))
 def number_of_users_callback(call):
+	markup=create_button_back()
 	bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id,
-		text=f"Количество пользователей: '{db_management.calculate_total_number_of_users()}'")
+		text=f"Количество пользователей: '{db_management.calculate_total_number_of_users()}'",
+		reply_markup=markup)
+
+def create_button_back():
+	markup = telebot.types.InlineKeyboardMarkup(row_width=1)
+	markup.add(
+		telebot.types.InlineKeyboardButton('Назад', callback_data='btn_back')
+	)
+	return markup
+
+@bot.callback_query_handler(func=lambda call: call.data.startswith('btn_back'))
+def button_back(call):
+	markup = main_menu()
+	bot.delete_message(call.message.chat.id,call.message.message_id)
+	bot.send_message(call.message.chat.id, "Вы вернулись в меню",reply_markup=markup)
+	return markup
+
 
 
 bot.polling(none_stop=True, interval=0)
--- db_management.py
+++ db_management.py
@@ -26,11 +26,11 @@ def user_is_admin(username):
 		cursor = db.cursor()
 		cursor.execute(f"SELECT is_admin FROM users WHERE name_of_user = '{username}'")
 		result = cursor.fetchall()
-
-		if result[0][0]:
-			return True
-		else:
+		print(result[0][0], "2")
+		if result[0][0] == "FALSE":
 			return False
+		else:
+			return True
 
 def hashing_of_name(username):
 	return ( (hashlib.md5(username.encode())).hexdigest() )
@@ -65,4 +65,5 @@ def update_statistics(username, command_name):
 		if(is_user_checking(hashing_of_name(str(username)))):
 
 			cursor.execute(f"UPDATE users SET '{command_name}' = '{get_value(username, command_name)}' + 1 WHERE name_of_user = '{hashing_of_name(str(username))}'")
-	print(2)
+
+
